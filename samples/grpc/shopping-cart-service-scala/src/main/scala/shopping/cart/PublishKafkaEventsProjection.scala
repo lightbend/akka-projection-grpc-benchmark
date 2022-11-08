c63835a69df640e85aea2828f24f06c60c0eeee0
Patrik Nordwall <patrik.nordwall@gmail.com>
2022-11-08 17:09:07 +0100

    Publish Kafka messages via flow (#7)
    * for higher throughput, see https://strimzi.io/blog/2020/10/15/producer-tuning/

--- samples/grpc/shopping-cart-service-scala/src/main/scala/shopping/cart/PublishKafkaEventsProjection.scala
+++ samples/grpc/shopping-cart-service-scala/src/main/scala/shopping/cart/PublishKafkaEventsProjection.scala
@@ -1,27 +1,32 @@
 package shopping.cart
 
-import akka.actor.CoordinatedShutdown
+import akka.Done
+import akka.NotUsed
 import akka.actor.typed.ActorSystem
 import akka.cluster.sharding.typed.ShardedDaemonProcessSettings
 import akka.cluster.sharding.typed.scaladsl.ShardedDaemonProcess
+import akka.kafka.ProducerMessage.Message
 import akka.kafka.ProducerSettings
-import akka.kafka.scaladsl.SendProducer
+import akka.kafka.scaladsl.Producer
 import akka.persistence.query.Offset
 import akka.persistence.query.typed.EventEnvelope
 import akka.persistence.r2dbc.query.javadsl.R2dbcReadJournal
+import akka.projection.Projection
+import akka.projection.ProjectionBehavior
+import akka.projection.ProjectionContext
+import akka.projection.ProjectionId
 import akka.projection.eventsourced.scaladsl.EventSourcedProvider
 import akka.projection.r2dbc.scaladsl.R2dbcProjection
-import akka.projection.scaladsl.{ AtLeastOnceProjection, SourceProvider }
-import akka.projection.{ ProjectionBehavior, ProjectionId }
-import org.apache.kafka.common.serialization.{
-  ByteArraySerializer,
-  StringSerializer
-}
+import akka.projection.scaladsl.SourceProvider
+import akka.stream.scaladsl.FlowWithContext
+import com.google.protobuf.any.{ Any => ScalaPBAny }
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.ByteArraySerializer
+import org.apache.kafka.common.serialization.StringSerializer
 
 object PublishKafkaEventsProjection {
 
   def init(system: ActorSystem[_]): Unit = {
-    val sendProducer = createProducer(system)
     val topic =
       system.settings.config.getString("shopping-cart-service.kafka.topic")
 
@@ -36,31 +41,15 @@ object PublishKafkaEventsProjection {
       numberOfInstances = sliceRanges.size,
       index =>
         ProjectionBehavior(
-          createProjectionFor(system, topic, sendProducer, sliceRanges(index))),
+          createProjectionFor(system, topic, sliceRanges(index))),
       ShardedDaemonProcessSettings(system),
       Some(ProjectionBehavior.Stop))
-  }
-
-  private def createProducer(
-      system: ActorSystem[_]): SendProducer[String, Array[Byte]] = {
-    val producerSettings =
-      ProducerSettings(system, new StringSerializer, new ByteArraySerializer)
-    val sendProducer =
-      SendProducer(producerSettings)(system)
-    CoordinatedShutdown(system).addTask(
-      CoordinatedShutdown.PhaseBeforeActorSystemTerminate,
-      "close-sendProducer") { () =>
-      sendProducer.close()
-    }
-    sendProducer
   }
 
   private def createProjectionFor(
       system: ActorSystem[_],
       topic: String,
-      sendProducer: SendProducer[String, Array[Byte]],
-      sliceRange: Range)
-      : AtLeastOnceProjection[Offset, EventEnvelope[ShoppingCart.Event]] = {
+      sliceRange: Range): Projection[EventEnvelope[ShoppingCart.Event]] = {
     val minSlice = sliceRange.min
     val maxSlice = sliceRange.max
     val projectionId =
@@ -75,13 +64,50 @@ object PublishKafkaEventsProjection {
         minSlice,
         maxSlice)
 
-    R2dbcProjection.atLeastOnceAsync(
+    val producerSettings =
+      ProducerSettings(system, new StringSerializer, new ByteArraySerializer)
+
+    val flow =
+      FlowWithContext[EventEnvelope[ShoppingCart.Event], ProjectionContext]
+        .map { envelope =>
+          val event = envelope.event
+          // using the cartId as the key and `DefaultPartitioner` will select partition based on the key
+          // so that events for same cart always ends up in same partition
+          val key = event.cartId
+          Message(
+            record = new ProducerRecord(
+              topic,
+              null,
+              envelope.timestamp,
+              key,
+              serialize(event)),
+            passThrough = NotUsed)
+        }
+        .via(Producer.flowWithContext(producerSettings))
+        .map(_ => Done)
+
+    R2dbcProjection.atLeastOnceFlow(
       projectionId,
       None,
       sourceProvider,
-      handler = () =>
-        new PublishKafkaEventsProjectionHandler(system, topic, sendProducer))(
-      system)
+      handler = flow)(system)
+  }
+
+  private def serialize(event: ShoppingCart.Event): Array[Byte] = {
+    val protoMessage = event match {
+      case ShoppingCart.ItemAdded(cartId, itemId, quantity) =>
+        proto.ItemAdded(cartId, itemId, quantity)
+
+      case ShoppingCart.ItemQuantityAdjusted(cartId, itemId, quantity, _) =>
+        proto.ItemQuantityAdjusted(cartId, itemId, quantity)
+      case ShoppingCart.ItemRemoved(cartId, itemId, _) =>
+        proto.ItemRemoved(cartId, itemId)
+
+      case ShoppingCart.CheckedOut(cartId, _) =>
+        proto.CheckedOut(cartId)
+    }
+    // pack in Any so that type information is included for deserialization
+    ScalaPBAny.pack(protoMessage, "shopping-cart-service").toByteArray
   }
 
 }
