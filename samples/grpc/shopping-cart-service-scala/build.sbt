738e00d9bc8ede4ddac1424ced3a1161ab4a1c5e
Johan Andrén <johan@markatta.com>
2022-12-23 16:56:31 +0100

    WIP replicated entity over grpc added

--- samples/grpc/shopping-cart-service-scala/build.sbt
+++ samples/grpc/shopping-cart-service-scala/build.sbt
@@ -27,13 +27,13 @@ run / javaOptions ++= sys.props
   .fold(Seq.empty[String])(res => Seq(s"-Dconfig.resource=$res"))
 Global / cancelable := false // ctrl-c
 
-val AkkaVersion = "2.7.0"
+val AkkaVersion = "2.8.0-M3"
 val AkkaHttpVersion = "10.4.0"
 val AkkaManagementVersion = "1.2.0"
 val AkkaPersistenceR2dbcVersion = "1.0.1"
 val AlpakkaKafkaVersion = "4.0.0"
 val AkkaProjectionVersion =
-  sys.props.getOrElse("akka-projection.version", "1.3.0")
+  sys.props.getOrElse("akka-projection.version", "1.3.0-37-9d102c58-SNAPSHOT")
 
 resolvers +=
   "Sonatype Snapshots".at(
--- samples/grpc/shopping-cart-service-scala/src/main/scala/shopping/cart/Main.scala
+++ samples/grpc/shopping-cart-service-scala/src/main/scala/shopping/cart/Main.scala
@@ -1,9 +1,19 @@
 package shopping.cart
 
 import akka.actor.typed.ActorSystem
+import akka.grpc.GrpcClientSettings
 import akka.management.cluster.bootstrap.ClusterBootstrap
 import akka.management.scaladsl.AkkaManagement
+import akka.persistence.typed.ReplicaId
+import akka.projection.grpc.consumer.GrpcQuerySettings
+import akka.projection.grpc.producer.EventProducerSettings
+import akka.projection.grpc.replication.Replica
+import akka.projection.grpc.replication.ReplicationSettings
+import akka.projection.grpc.replication.scaladsl.Replication
+import com.typesafe.config.Config
 import org.slf4j.LoggerFactory
+
+import scala.jdk.CollectionConverters._
 import scala.util.control.NonFatal
 
 object Main {
@@ -26,18 +36,29 @@ object Main {
     AkkaManagement(system).start()
     ClusterBootstrap(system).start()
 
-    ShoppingCart.init(system)
-    if (system.settings.config.getBoolean(
-        "shopping-cart-service.kafka.enabled"))
-      PublishKafkaEventsProjection.init(system)
+    val (eventProducerHandler, entityKey) =
+      if (system.settings.config.getBoolean(
+          "shopping-cart-service.replication.enabled")) {
+        // Replicated Event Sourcing Shopping cart
+        val replication = setUpReplication()(system)
+        (replication.createSingleServiceHandler(), replication.entityTypeKey)
+      } else {
+        // Regular Event Sourced shopping cart
+        ShoppingCart.init(system)
+        if (system.settings.config.getBoolean(
+            "shopping-cart-service.kafka.enabled"))
+          PublishKafkaEventsProjection.init(system)
 
-    val eventProducerService = PublishEvents.eventProducerService(system)
+        (PublishEvents.eventProducerService(system), ShoppingCart.EntityKey)
+      }
 
     val grpcInterface =
       system.settings.config.getString("shopping-cart-service.grpc.interface")
     val grpcPort =
       system.settings.config.getInt("shopping-cart-service.grpc.port")
-    val shoppingCartService = new ShoppingCartServiceImpl(system)
+
+    val shoppingCartService =
+      new ShoppingCartServiceImpl(entityKey, system)
     val simulatorService = new SimulatorServiceImpl(system)
     ShoppingCartServer.start(
       grpcInterface,
@@ -45,7 +66,40 @@ object Main {
       system,
       shoppingCartService,
       simulatorService,
-      eventProducerService)
+      eventProducerHandler)
+  }
+
+  private def setUpReplication()(
+      implicit system: ActorSystem[_]): Replication[ShoppingCart.Command] = {
+    // FIXME from config
+    val selfReplicaId = ReplicaId(
+      system.settings.config
+        .getString("shopping-cart-service.replication.self-replica-id"))
+
+    val allReplicas = system.settings.config
+      .getConfigList("shopping-cart-service.replication.replicas")
+      .asScala
+      .toSet
+      .map { config: Config =>
+        val replicaId = config.getString("replica-id")
+        Replica(
+          ReplicaId(replicaId),
+          numberOfConsumers = config.getInt("number-of-consumers"),
+          querySettings = GrpcQuerySettings(system), // FIXME ok to share?
+          // so akka.grpc.client.[replica-id]
+          grpcClientSettings = GrpcClientSettings.fromConfig(replicaId))
+      }
+
+    val replicationSettings = ReplicationSettings[ShoppingCart.Command](
+      "cart",
+      selfReplicaId,
+      EventProducerSettings(system),
+      allReplicas.filter(_.replicaId != selfReplicaId))
+    Replication.grpcReplication[
+      ShoppingCart.Command,
+      ReplicatedShoppingCart.Event,
+      ReplicatedShoppingCart.State](replicationSettings)(
+      ReplicatedShoppingCart.apply)(system)
   }
 
 }
